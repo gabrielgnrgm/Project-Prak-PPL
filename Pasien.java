6ebed846b73601af7312504ab4f27d8008188314
gabrielgnrgm <73882038+gabrielgnrgm@users.noreply.github.com>
2020-11-07 23:07:09 +0700

    Update Pasien.java

--- Pasien.java
+++ Pasien.java
@@ -1,11 +1,41 @@
 package Aplikasi;
 
 public class Pasien {
-    String id;
-    String password; 
-    String nama;
+    private String nik;
+    private String password;
+    private String nama;
+    private String email;
+  
     
-    public void gantiPassword(String newPass){
-        password=newPass;
+    public Pasien(String nama,String nik,String email,String password){
+       this.nik=nik;
+       this.nama=nama;
+       this.email=email;
+       this.password=password;
     }
+//set Method    
+    public void gantiPassword(String newPass){
+        this.password=newPass;
+    }
+    public void gantiEmail(String email){
+        this.email=email;
+    }
+//get Method    
+    public String getiD(){
+        return nik;
+    }
+    
+    public String getNama(){
+       return nama; 
+    }
+    
+    public String getEmail(){
+        return email;
+    }
+    
+    public String getPassword(){
+        return password;
+    } 
+    
+    
 }
