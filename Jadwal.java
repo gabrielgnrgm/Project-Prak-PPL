40446b425d1d5cc40279e927cdc9bcd5c61d2ee1
Rafel Siregar <50065143+rafelsiregar@users.noreply.github.com>
2020-11-09 18:12:03 +0700

    Create Jadwal.java

---
+++ Jadwal.java
@@ -0,0 +1,12 @@
+package Aplikasi;
+import java.util.Date;
+
+public class Jadwal{
+  public enum Day {
+    Senin, Selasa, Rabu, Kamis, Jumat, Sabtu, Minggu
+  }
+  private int id;
+  private Day hari;
+  private String clock;
+
+}
