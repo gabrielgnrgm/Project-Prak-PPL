5b4bdf0e70591030e06e312e1e3170db195cf731
gabrielgnrgm <73882038+gabrielgnrgm@users.noreply.github.com>
2020-11-09 20:50:16 +0700

    Update Psikiater.java

--- Psikiater.java
+++ Psikiater.java
@@ -1,15 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Aplikasi;
-
-/**
- *
- * @author USER
- */
-public class Psikiater {
+public class Psikiater extends User{
     private int id_psikiater;
     private String nama;
     private String alamat;
