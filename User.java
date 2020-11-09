6d1fe31c1d538e0c075343c2bcac11b012c171c3
Rafel Siregar <50065143+rafelsiregar@users.noreply.github.com>
2020-11-09 08:14:43 +0700

    Create User.java

---
+++ User.java
@@ -0,0 +1,13 @@
+public class User {
+  private int user_id;
+  private String email, username, password;
+  private 
+  
+  public User(String email, String username, String password, String role){
+    this.id=id;
+    this.username=username;
+    this.password=password;
+    this.role=role;
+  }
+
+}
