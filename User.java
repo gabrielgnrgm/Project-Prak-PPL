5494dc8f9910ff5408ec872ba8564d7e59548f8a
Rafel Siregar <50065143+rafelsiregar@users.noreply.github.com>
2020-11-09 08:19:50 +0700

    Update User.java

--- User.java
+++ User.java
@@ -3,11 +3,22 @@ public class User {
   private String email, username, password;
   private 
   
-  public User(String email, String username, String password, String role){
+  public User(int id, String email, String username, String password, String role){
     this.id=id;
     this.username=username;
     this.password=password;
     this.role=role;
   }
 
+  public void loginWithUsername(String username, String password){
+    if(username==this.username && password==this.password){
+      //Apa ini? Aku bingung?
+    }
+  }
+  
+  public void loginWithEmail(String email, String password){
+    if(email==this.email && password==this.password){
+      //Apa ini? Aku bingung
+    }
+  }
 }
