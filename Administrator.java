c197dbb94d75cbc08f73fda2218544bbebb1b7ac
Rafel Siregar <50065143+rafelsiregar@users.noreply.github.com>
2020-11-09 22:12:57 +0700

    Create Administrator.java

---
+++ Administrator.java
@@ -0,0 +1,20 @@
+public class Administrator extends User {
+  private int id;
+  private String nama;
+
+  /*public Administrator(){
+  Perlu constructor gak?
+  }*/
+  
+  public int getId(){
+    return id;
+  }
+  
+  public String getNama(){
+    return nama;
+  }
+  
+  public void deleteUser(){
+    //Code to delete user
+  }
+}
