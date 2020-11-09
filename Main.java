1664f1ddd0cbe0b655a84f59af8a6dab81f388af
gabrielgnrgm <73882038+gabrielgnrgm@users.noreply.github.com>
2020-11-09 20:43:47 +0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,21 +1,56 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Aplikasi;
-
-/**
- *
- * @author USER
- */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static Pasien[] loadDataPasien(String path){
+        File file=new File(path);
+        Pasien[] pasien=new Pasien[10];
+
+        Scanner inStream;
+        
+      //Parse Csv to java: 
+        try{
+            inStream = new Scanner(file);
+ 
+            int LNidx=0;
+            
+         while(inStream.hasNext()){// per satu baris
+                String line= inStream.next();
+                String[] val=line.split(",");
+                
+
+                    //9 indexes: (id,Nama,Email) (username,password,role) (Gender,NIK, Alamat)
+                        int id=Integer.parseInt(val[0]);
+                        String nama=val[1];
+                        String email=val[2];
+                        
+                        String username=val[3];
+                        String password=val[4];
+                        //String role=val[5];
+                        String gender=val[6];
+                        int nik=Integer.parseInt(val[7]);
+                        String alamat=val[8];
+                        
+                   
+                        //(String nama,int nik,String alamat,String jenis_kelamin     
+                        pasien[LNidx]=new Pasien(nama,nik,alamat,gender);
+                        pasien[LNidx].setAccount(id,email, username, password,"Pasien");
+                        LNidx++;  
+ 
+        }
+
+         inStream.close();
+         System.out.println("Data pasien berhasil dimasukkan");
+         return pasien;
+         
+        
+     }catch(FileNotFoundException e){
+     return null;}
+
+    }
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        Pasien[] pasien=new Pasien[10];
+        pasien=loadDataPasien("Pasien.csv");
     }
     
 }
